1e55e9afe23a4019ccb775d1a7d481e01748b45a
Manh.D <07mandao@skola.boras.se>
2024-05-24 20:12:59 +0200

    lagt in en footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,25 @@ import Hero from "@/components/Hero";
 import { GlobeDemo } from "@/components/Globe-section";
 import { BentoGridThirdDemo } from "@/components/bento-section";
 import { StickyScrollRevealDemo } from "@/components/Logisitk-kedja";
-import FooterSection from "@/components/footer";
+import {FooterSection} from "@/components/footer";
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <div>
+     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 pb-28">
       <div className="max-w-7xl w-full">
         <Hero/>
         <StickyScrollRevealDemo/>
         <GlobeDemo/>
         <BentoGridThirdDemo/>
-        <FooterSection/>
       </div>
     </main>
+    <footer>
+      <FooterSection/>
+    </footer>
+
+    </div>
+
+
 
   );
 } 
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
 
-const FooterSection = () => {
+"use client";
+
+import { Footer } from "flowbite-react";
+import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+
+export function FooterSection() {
   return (
-    <div>footsssser</div>
-  )
+    <Footer container>
+      <div className="w-full bg-black p-10">
+        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+          <div>
+            <Footer.Brand
+              href="#"
+              src="https://flowbite.com/docs/images/logo.svg"
+              alt="Sigma Logo"
+              name="Team Sigma"
+            />
+          </div>
+          <div className=" mb-5 grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="Om oss" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Värderingar</Footer.Link>
+                <Footer.Link href="#">Hållbarhet</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Github</Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between mt-10">
+          <Footer.Copyright href="#" by="Team Sigma™" year={2026} />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsTwitter} />
+            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
 }
-
-export default FooterSection
