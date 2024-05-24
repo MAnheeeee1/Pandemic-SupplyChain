d8f022cde629848ca35e3303f9dea5096ac51800
Manh.D <07mandao@skola.boras.se>
2024-05-24 19:49:30 +0200

    lagt in information på logsik kedjan

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import { GlobeDemo } from "@/components/Globe-section";
 import { BentoGridThirdDemo } from "@/components/bento-section";
 import { StickyScrollRevealDemo } from "@/components/Logisitk-kedja";
+import FooterSection from "@/components/footer";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
@@ -10,6 +11,7 @@ export default function Home() {
         <StickyScrollRevealDemo/>
         <GlobeDemo/>
         <BentoGridThirdDemo/>
+        <FooterSection/>
       </div>
     </main>
 
---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FooterSection = () => {
+  return (
+    <div>footsssser</div>
+  )
+}
+
+export default FooterSection
