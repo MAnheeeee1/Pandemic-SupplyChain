e1adf1f4499b9bd86507af092ea39b811b60495c
Manh.D <07mandao@skola.boras.se>
2024-06-01 13:21:41 +0200

    fixat scrollen på logsyikkedjan

--- components/ui/sticky-scroll-reveal.tsx
+++ components/ui/sticky-scroll-reveal.tsx
@@ -19,8 +19,8 @@ export const StickyScroll = ({
   const ref = useRef<any>(null);
   const { scrollYProgress } = useScroll({
     // uncomment line 22 and comment line 23 if you DONT want the overflow container and want to have it change on the entire page scroll
-    // target: ref
-    container: ref,
+    target: ref,
+    //container: ref,
     offset: ["start start", "end start"],
   });
   const cardLength = content.length;
